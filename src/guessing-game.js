60e8856b886cae77354ef26f49771598db6e0804
Alexei Torchinsky <alextor1005@gmail.com>
2023-10-02 12:06:27 +0300

    feat: done guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
-
+			return this.num = Math.ceil((this.min + this.max)/ 2);
     }
 
     lower() {
-
+      this.max = this.num; 
+			
     }
 
     greater() {
-
+			this.min = this.num;
+			
     }
 }
+
 
 module.exports = GuessingGame;
